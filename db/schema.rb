e27727a1cf7038932bb081e9d53b5e06ca29cbac
solybarr <solybarr@gmail.com>
2017-01-03 20:01:23 -0500

    4.6 Dynamic App - Revised with Price

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027163608) do
+ActiveRecord::Schema.define(version: 20170104002842) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20161027163608) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
+    t.decimal  "price"
   end
 
 end
